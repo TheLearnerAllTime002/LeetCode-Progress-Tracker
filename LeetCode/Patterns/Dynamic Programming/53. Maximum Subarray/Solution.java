601a5fae1ee4718db4494930aadc10bbbba72dc3
Arjun Mitra <arjunmitra06082004@gmail.com>
2026-08-11 00:30:41 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/53. Maximum Subarray/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/53. Maximum Subarray/Solution.java
@@ -3,8 +3,8 @@ class Solution {
         int maxi = nums[0];
         int saxi = nums[0];
 
-        for(int i = 1; i < nums.length; i++){
-            saxi = Math.max(nums[i], nums[i]+saxi);
+        for (int i = 1; i < nums.length; i++) {
+            saxi = Math.max(nums[i], nums[i] + saxi);
             maxi = Math.max(saxi, maxi);
         }
         return maxi;
