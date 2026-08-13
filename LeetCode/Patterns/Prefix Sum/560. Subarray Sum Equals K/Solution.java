3c68282c5f158daf44ae4afaf79db96922dd155c
Arjun Mitra <arjunmitra06082004@gmail.com>
2026-08-13 09:26:45 +0530

    Add Java Solution to Patterns/Prefix Sum

--- LeetCode/Patterns/Prefix Sum/560. Subarray Sum Equals K/Solution.java
+++ LeetCode/Patterns/Prefix Sum/560. Subarray Sum Equals K/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map <Integer, Integer> SumCountMap = new HashMap<>();
-        SumCountMap.put(0,1);
+        Map<Integer, Integer> SumCountMap = new HashMap<>();
+        SumCountMap.put(0, 1);
 
         int result = 0;
         int prefixsum = 0;
 
-        for(int num: nums){
+        for (int num : nums) {
             prefixsum += num;
-            if(SumCountMap.containsKey(prefixsum - k)){
-                result += SumCountMap.get(prefixsum -k);
+            if (SumCountMap.containsKey(prefixsum - k)) {
+                result += SumCountMap.get(prefixsum - k);
             }
-            SumCountMap.put (prefixsum, SumCountMap.getOrDefault(prefixsum, 0)+1);
+            SumCountMap.put(prefixsum, SumCountMap.getOrDefault(prefixsum, 0) + 1);
         }
         return result;
     }
