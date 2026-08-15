d458c38f6977039d83acb861fe3622c63f1ad1ba
Arjun Mitra <arjunmitra06082004@gmail.com>
2026-08-15 09:41:41 +0530

    Add Java Solution to Patterns/Bit Manipulation

--- LeetCode/Patterns/Bit Manipulation/3702. Longest Subsequence With Non-Zero Bitwise XOR/Solution.java
+++ LeetCode/Patterns/Bit Manipulation/3702. Longest Subsequence With Non-Zero Bitwise XOR/Solution.java
@@ -4,18 +4,19 @@ class Solution {
         int count = 0;
         int n = nums.length;
 
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             XOR ^= nums[i];
-            if(nums[i] == 0){
+            if (nums[i] == 0) {
                 count++;
             }
         }
 
-        if(XOR != 0){
+        if (XOR != 0) {
             return n;
-        }else{
-            if(count == n) return 0;
+        } else {
+            if (count == n)
+                return 0;
         }
-        return n-1; 
+        return n - 1;
     }
 }
