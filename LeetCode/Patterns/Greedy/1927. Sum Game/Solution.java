6527a82356c4a6becda80ecbd7e4430d090884fe
Arjun Mitra <arjunmitra06082004@gmail.com>
2026-08-23 09:32:55 +0530

    Add Java Solution to Patterns/Greedy

---
+++ LeetCode/Patterns/Greedy/1927. Sum Game/Solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean sumGame(String num) {
+
+        int n = num.length();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        // First half
+        for (int i = 0; i < half; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
+            }
+        }
+
+        // Second half
+        for (int i = half; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
+            }
+        }
+
+        int diff = Math.abs(leftSum - rightSum);
+        int qDiff = Math.abs(leftQ - rightQ);
+
+        return diff != 9 * qDiff / 2;
+    }
+}
