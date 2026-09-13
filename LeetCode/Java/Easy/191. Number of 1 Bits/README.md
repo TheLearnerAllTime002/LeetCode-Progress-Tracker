995bfb3f2687b7b05173b9245632f6a12b20cedf
Arjun Mitra <arjunmitra06082004@gmail.com>
2026-09-13 13:24:02 +0530

    Add Easy Java Solution for 191. Number of 1 Bits from LeetCode

--- LeetCode/Java/Easy/191. Number of 1 Bits/README.md
+++ LeetCode/Java/Easy/191. Number of 1 Bits/README.md
@@ -8,8 +8,8 @@
 Divide and Conquer, Bit Manipulation
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 42.6 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Java/Easy/191. Number of 1 Bits/Solution.java
+++ LeetCode/Java/Easy/191. Number of 1 Bits/Solution.java
@@ -1,10 +1,6 @@
 class Solution {
     public int hammingWeight(int n) {
-        int count = 0;
-        while(n != 0){
-            count += (n%2);
-            n = n/2;
-        }
-        return count;
+        
+        return Integer.bitCount(n);
     }
 }
