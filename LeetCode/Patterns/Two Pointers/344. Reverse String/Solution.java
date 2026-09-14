5f11e1f676e91da446c383d6ee6637dff7d7c1b5
Arjun Mitra <arjunmitra06082004@gmail.com>
2026-09-14 16:45:17 +0530

    Add Easy Java Solution for 344. Reverse String from LeetCode

---
+++ LeetCode/Patterns/Two Pointers/344. Reverse String/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length -1;
+
+        while(left<right){
+            s[left] = (char)(s[left] ^ s[right]);
+            s[right] = (char)(s[left] ^ s[right]);
+            s[left] = (char)(s[left] ^ s[right]);
+
+            left++;
+            right--;
+        }
+    }
+}
